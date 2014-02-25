312dbb708dc198849ace6dc57582d81f5f06e2f0
Alexander Wood & Bailey Drake <student@epicodus.com>
2014-02-25 09:42:24 -0800

    has nice formatting

--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,11 @@
-describe('Contact', function() {
-  describe('fullName', function() {
-    it("returns the first and last names concatenated with a space and the address", function() {
-      var testContact = Object.create(Contact);
-      testContact.firstName = "Dolly";
-      testContact.lastName = "Parton";
-      testContact.address = "LA"
-      testContact.fullName().should.equal("Dolly Parton");
+describe("Address", function() {
+  describe("fullAddress", function() {
+    it("returns the full address with nice formatting", function() {
+      var testAddress = Object.create(Address);
+      testAddress.street = "123 4th Ave";
+      testAddress.city = "Portland";
+      testAddress.state = "Oregon";
+      testAddress.fullAddress().should.equal("123 4th Ave, Portland, Oregon");
     });
   });
 });
